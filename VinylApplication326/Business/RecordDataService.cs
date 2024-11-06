2d3df4b58eefc1cfe608e9828efd16c13d109148
peyholle@gmail.com <peyholle@gmail.com>
2024-11-05 21:47:16 -0700

    Browse screen working.

---
+++ VinylApplication326/Business/RecordDataService.cs
@@ -0,0 +1,27 @@
+﻿using VinylApplication326.DAO;
+using VinylApplication326.Models;
+
+namespace VinylApplication326.Services
+{
+    public class RecordDataService
+    {
+        RecordDao dao;
+
+        public RecordDataService()
+        {
+            dao = new RecordDao();
+        }
+
+        public List<RecordModel> readRecords() {
+
+            List<RecordModel> records = dao.GetRecords();
+            
+            return records;
+        }
+
+        public void favoriteRecord(int recordId)
+        {
+            dao.FavoriteToggle(recordId);
+        }
+    }
+}
